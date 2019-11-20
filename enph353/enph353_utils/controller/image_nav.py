10a9c60c36131aa9d8ed58a807064bb94285f530
juliarosenrauch <jul_rose@yahoo.ca>
2019-11-20 14:40:56 -0800

    [major] got initial turn workinggit add image_nav.py next step: car recognition

--- enph353/enph353_utils/controller/image_nav.py
+++ enph353/enph353_utils/controller/image_nav.py
@@ -20,7 +20,7 @@ class image_converter:
         self.vel_pub = rospy.Publisher("R1/cmd_vel", Twist, queue_size=30)
         self.prev_err = 0
         self.crosswalk = 0
-        self.red = 0
+        self.state = -3
 
     def callback(self, data):
         try:
@@ -32,34 +32,80 @@ class image_converter:
         width = np.size(frame,1)
         #print(height, width)
 
+        #### frame processing for right line following
         roi_right = frame[height-300:height, width-640:width] #region of interest
         gray = cv.cvtColor(roi_right, cv.COLOR_BGR2GRAY)
         ret, thresh = cv.threshold(gray, 127, 255, cv.THRESH_BINARY_INV)
-
-        roi_crosswalk = frame[height-200:height] # look at 50 rows of frame towards the bottom of screen
-
-        lower_red = np.array([0,0,245])
-        upper_red = np.array([10,10,255])
-    
-        mask_red = cv.inRange(roi_crosswalk, lower_red, upper_red)
-        #cv.imshow("Crosswalk detection", mask_red)
 
         M = cv.moments(thresh)
         meep = (M["m00"])
         if meep < 1:
             meep = meep + 1
         cX = int(M["m10"] / meep)+640
-        
+
         cv.circle(frame, (int(cX), height-100), 20, (0, 255, 0), -1)
         #cv.imshow("Cropped right corner", gray)
-        cv.imshow("Robot Camera", frame)
-        cv.waitKey(1)
 
         drive_error = cX - 960
         #print("drive error:", drive_error)
         velocity = Twist()
 
-        if (self.red == 0) or (self.red == 5):
+        #### frame processing for red line detection
+        roi_crosswalk = frame[height-200:height] # look at 50 rows of frame towards the bottom of screen
+        lower_red = np.array([0,0,245])
+        upper_red = np.array([10,10,255])
+        mask_red = cv.inRange(roi_crosswalk, lower_red, upper_red)
+        #cv.imshow("Crosswalk detection", mask_red)
+
+        #### frame processing for initial turn
+        roi_init = frame[0:height, 0:width]
+        # gray_init = cv.cvtColor(roi_init, cv.COLOR_BGR2GRAY)
+        # ret_init, thresh_init = cv.threshold(gray_init, 127, 255, cv.THRESH_BINARY_INV)
+        # cv.imshow("Initial Turn", thresh_init)
+        lower_white = np.array([190,190,190])
+        upper_white = np.array([255,255,255])
+        mask_init = cv.inRange(roi_init, lower_white, upper_white)
+        cv.imshow("Initial Turn", mask_init)
+
+
+        #cv.imshow("Robot Camera", frame)
+        cv.waitKey(1)
+        ########### STATE MACHINE #######################
+
+        # inital turn to get to outer loop
+        if (self.state == -3):
+            sum = 0
+            for i in range (620, 660):
+                for j in range (10, 500):
+                    sum += mask_init[j][i] #numpy arrays are y,x
+
+            if sum > 0:
+                print("DRIVING STRAIGHT")
+                velocity.angular.z = 0
+                self.vel_pub.publish(velocity)
+                velocity.linear.x = speed
+                self.vel_pub.publish(velocity)
+            else:
+                self.state = -2
+
+        elif (self.state == -2):
+            # turn left until good enough to line follow
+            sum = 0
+            for i in range (500, 780):
+                for j in range (300, 400):
+                    sum += mask_init[j][i] #numpy arrays are y,x
+
+            cv.rectangle(mask_init, (490, 290), (790, 390), (255,0,0), 1)
+            if sum == 0:
+                print("TURNING")
+                velocity.angular.z = d
+                self.vel_pub.publish(velocity)
+            else:
+                print("GOING TO REGULAR DRIVING")
+                self.state = 0
+
+        # regular following
+        elif (self.state == 0) or (self.state == 5):
             if (drive_error > 3) or (drive_error < -3):
                 velocity.angular.z = d * drive_error
                 #print("angular v:", velocity.angular.z)
@@ -68,10 +114,9 @@ class image_converter:
                 velocity.linear.x = speed
                 self.vel_pub.publish(velocity)
 
-
-        #self.red == 0 havent seen anything
+        #self.state == 0 havent seen anything
         # look for red
-        # when u see red, set self.red to 1 (saw the red line transition to next step)
+        # when u see red, set self.state to 1 (saw the red line transition to next step)
         # pause for 5 seconds
             sum = 0
             for i in range (400,800):
@@ -81,18 +126,18 @@ class image_converter:
             if sum > 0:
                 print("SEEING RED 1:", sum)
                 self.crosswalk += 1
-                self.red = -1
+                self.state = -1
                 velocity.angular.z = 0
                 self.vel_pub.publish(velocity)
                 velocity.linear.x = 0
                 self.vel_pub.publish(velocity)
 
-        elif self.red == -1:
+        elif self.state == -1:
             # we look for pedestrian
             print("looking for ped")
             self.crosswalk += 1
 
-            roi_ped = frame[0:500, 0:width] 
+            roi_ped = frame[0:500, 0:width]
 
             lower_white = np.array([190,190,190])
             upper_white = np.array([255,255,255])
@@ -113,16 +158,15 @@ class image_converter:
                 self.vel_pub.publish(velocity)
                 velocity.linear.x = 0
                 self.vel_pub.publish(velocity)
-            
-            else: 
-                self.red = 1
 
+            else:
+                self.state = 1
 
-        # self.red == 1 saw the first red line
+        # self.state == 1 saw the first red line
         # need to wait for that region to be black again
-        # drive forward untili sum =0 
-        # if sum = 0, set self.red to 2 (saw black transition to next)
-        elif self.red == 1:
+        # drive forward untili sum =0
+        # if sum = 0, set self.state to 2 (saw black transition to next)
+        elif self.state == 1:
             sum = 0
             for i in range (400,800):
                 for j in range (0,20):
@@ -130,18 +174,17 @@ class image_converter:
 
             if sum == 0:
                 print("SEEING BLACK 2:", sum)
-                self.red = 2
+                self.state = 2
             else:
                 velocity.angular.z = 0
                 self.vel_pub.publish(velocity)
                 velocity.linear.x = speed
                 self.vel_pub.publish(velocity)
 
-
-        # self.red == 2 in between two red lines looking for second
+        # self.state == 2 in between two red lines looking for second
         # drive straight until sum > 0
-        # if sum > 0 then set self.red to 3 (saw the second red line)
-        elif self.red == 2:
+        # if sum > 0 then set self.state to 3 (saw the second red line)
+        elif self.state == 2:
             sum = 0
             for i in range (400,800):
                 sum += mask_red[20][i] #numpy arrays are y,x
@@ -153,27 +196,27 @@ class image_converter:
                 self.vel_pub.publish(velocity)
             else:
                 print("SEEING RED 3:", sum)
-                self.red = 3
+                self.state = 3
 
-        # self.red == 3 saw second red line
+        # self.state == 3 saw second red line
         # drive straight until black again
-        # set self.red to 0 again ( we are past the crosswalk )
+        # set self.state to 0 again ( we are past the crosswalk )
         # then go back to ur regular driving
-        elif self.red == 3:
+        elif self.state == 3:
             sum = 0
             for i in range (400,800):
                 sum += mask_red[20][i] #numpy arrays are y,x
 
             if sum == 0:
                 print("SEEING BLACK 4", sum)
-                self.red = 4
+                self.state = 4
             else:
                 velocity.angular.z = 0
                 self.vel_pub.publish(velocity)
                 velocity.linear.x = speed
                 self.vel_pub.publish(velocity)
 
-        elif self.red == 4:
+        elif self.state == 4:
             if (drive_error > 3) or (drive_error < -3):
                 #print("drive_error:", drive_error)
                 velocity.angular.z = d * drive_error
@@ -185,18 +228,18 @@ class image_converter:
 
             if drive_error > 7:
                 print("SWITCHING TO STATE 5")
-                self.red = 5
+                self.state = 5
 
         # if self.crosswalk == 3:
         #     velocity.angular.z = 0
         #     self.vel_pub.publish(velocity)
         #     velocity.linear.x = 0
         #     self.vel_pub.publish(velocity)
-            
+
 def main(args):
     # nav = navigation()
     # rospy.init_node('navigation', anonymous=True)
-    
+
     # rospy.Subscriber('right_follow', navigation.right_follow_callback, queue_size=1)
     # rospy.Subscriber('cross_walk', navigation.crosswalk_callback, queue_size=1)
     ic = image_converter()
@@ -209,4 +252,3 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv)
-    
